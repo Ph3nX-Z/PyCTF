88e0abcdf5fd5e7a8491b68fa9641798de6c7ac8
Ph3nX-Z <66122220+Ph3nX-Z@users.noreply.github.com>
2022-02-07 11:39:28 +0100

    Add files via upload

--- libs/docker.py
+++ libs/docker.py
@@ -1,1 +1,32 @@
 import os
+
+def execute_cmd(cmd):
+    return "".join(list(os.popen(cmd)))
+
+def list_dockers():
+    return execute_cmd("docker container ls")
+
+def docker_build(file):
+    out = execute_cmd(f"docker build - <{file}")
+    for i in out.split("\n"):
+        if "Successfully built" in i:
+            return i.split()[2]
+
+def delete_all():
+    return execute_cmd('docker system prune -a --force')
+
+def delete_container(id):
+    try:
+        execute_cmd(f"docker stop {id}")
+    except:
+        pass
+    try:
+        execute_cmd(f"docker rm {id}")
+    except:
+        raise "Failed"
+
+if __name__=="__main__":
+    delete_all()
+    print(docker_build("./dockerfiles/Test"))
+    list_dockers()
+    #delete_container("bfba93f9dbbb")
---
+++ libs/graphs.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from math import pi
+import matplotlib.pyplot as plt
+ 
+# Set data
+df = pd.DataFrame({
+'group': ['A'],
+'network': [5],
+'web': [5],
+'pwn': [1],
+'ctf': [2],
+'crypto': [8]
+})
+ 
+# number of variable
+categories=list(df)[1:]
+N = len(categories)
+ 
+# We are going to plot the first line of the data frame.
+# But we need to repeat the first value to close the circular graph:
+values=df.loc[0].drop('group').values.flatten().tolist()
+values += values[:1]
+values
+ 
+# What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+angles = [n / float(N) * 2 * pi for n in range(N)]
+angles += angles[:1]
+ 
+# Initialise the spider plot
+ax = plt.subplot(111, polar=True)
+ 
+# Draw one axe per variable + add labels
+plt.xticks(angles[:-1], categories, color='grey', size=8)
+ 
+# Draw ylabels
+ax.set_rlabel_position(0)
+plt.yticks([10,20,30], ["10","20","30"], color="grey", size=7)
+plt.ylim(0,40)
+ 
+# Plot data
+ax.plot(angles, values, linewidth=1, linestyle='solid')
+ 
+# Fill area
+ax.fill(angles, values, 'b', alpha=0.1)
+
+# Show the graph
+#plt.show()
+plt.savefig('graph.png',transparent=True)
